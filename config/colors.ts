64b2f2a739cbc63748235540c8667fd2274f4b38
Damien Vesper <ldamienvesper@gmail.com>
2021-07-24 17:07:06 -0500

    add colors config

---
+++ config/colors.ts
@@ -0,0 +1,88 @@
+/**
+ * Hypixel Rank Configuration
+ * @author mdashlw
+ */
+const RANKS = {
+    NORMAL: {
+        color: `§7`,
+        prefix: `§7`
+    },
+
+    VIP: {
+        color: `§a`,
+        prefix: `§a[VIP] `
+    },
+
+    VIP_PLUS: {
+        color: `§a`,
+        prefix: `§a[VIP§6+§a] `
+    },
+
+    MVP: {
+        color: `§b`,
+        prefix: `§b[MVP] `
+    },
+
+    MVP_PLUS: {
+        color: `§b`,
+        prefix: `§b[MVP$+§b] `
+    },
+
+    SUPERSTAR: {
+        color: `§6`,
+        prefix: `@[MVP$++@] `
+    },
+
+    YOUTUBER: {
+        color: `§c`,
+        prefix: `§c[§fYOUTUBE§c] `
+    },
+
+    JR_HELPER: {
+        color: `§9`,
+        prefix: `§9[JR HELPER] `
+    },
+
+    HELPER: {
+        color: `§9`,
+        prefix: `§9[HELPER] `
+    },
+
+    MODERATOR: {
+        color: `§2`,
+        prefix: `§2[MOD] `
+    },
+
+    ADMIN: {
+        color: `§c`,
+        prefix: `§c[ADMIN] `
+    }
+};
+
+/**
+ * Minecraft Color Configuration
+ * @author mdashlw
+ */
+const COLORS = {
+    BLACK: `§0`,
+    DARK_BLUE: `§1`,
+    DARK_GREEN: `§2`,
+    DARK_AQUA: `§3`,
+    DARK_RED: `§4`,
+    DARK_PURPLE: `§5`,
+    GOLD: `§6`,
+    GRAY: `§7`,
+    DARK_GRAY: `§8`,
+    BLUE: `§9`,
+    GREEN: `§a`,
+    AQUA: `§b`,
+    RED: `§c`,
+    LIGHT_PURPLE: `§d`,
+    YELLOW: `§e`,
+    WHITE: `§f`
+};
+
+export {
+    RANKS,
+    COLORS
+};
