5ab5b73a37a0d916686210f2f1a0862c1cf7c3b3
Damien Vesper <ldamienvesper@gmail.com>
2021-07-24 16:54:19 -0500

    add a conf

---
+++ config/config.ts
@@ -0,0 +1,9 @@
+/**
+ * The configuration file for the OBS overlay.
+ */
+const config = {
+    apiKey: ``, // Use /api in Hypixel to get your key. Don't share this with anyyone!
+    player: `` // Enter your Minecraft UUID here.
+};
+
+export default config;
